fb8883358d3e48afc2a3e487396aa4163f13b789
Rodrigo Anchieta <rodrigoganchieta@gmail.com>
2023-05-02 19:49:27 -0300

    first commit

---
+++ api/posts/models/posts.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const axios = require('axios');
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+  // lifecycles: {
+  //   async afterCreate(result, data) {
+  //     axios.post('https://api.netlify.com/build_hooks/5f7b9440bd98af634ec040f3');
+  //   },
+
+  //   async afterUpdate(result, params, data) {
+  //     axios.post('https://api.netlify.com/build_hooks/5f7b9440bd98af634ec040f3');
+  //   },
+  // }
+};
